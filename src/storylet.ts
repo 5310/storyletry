11e4cd3c0b90f69cbba88645d0d2a3142f5dac7c
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2020-03-15 13:40:48 +0530

    Restructured storylet

--- src/storylet.ts
+++ src/storylet.ts
@@ -1,32 +1,35 @@
 import { PRNG } from './prng'
 
-export type Context<Content> = {
+export type Context = {
   prng: PRNG,
   state: object,
-  path: number[],
-}
-export type Reading<Content> = {
-  state: object,
-  content: Content[],
-  interrupt?: Choice<Content> | boolean,
+  index: number[],
+  response: unknown,
 }
 export type Choice<Content> = {
   slug: Content,
-  choice: number,
+  index: number,
 }
-export type Test<Content> = (context: Context<Content>) => number
-export type Read<Content> = (context: Context<Content>) => Reading<Content>
+export type End = 'END'
+export type Reading<Content, Question> = {
+  state: object,
+  story: Content[],
+  index?: number[],
+  request?: End | Choice<Content> | Question,
+}
+export type Test = (context: Context) => number
+export type Read<Content, Question> = (context: Context) => Reading<Content, Question>
 
-export default class Storylet<Content> {
-  test: Test<Content>
-  read: Read<Content>
+export default class Storylet<Content, Question> {
+  test: Test
+  read: Read<Content, Question>
 
-  constructor(test: Test<Content>, read: Read<Content>) {
+  constructor(test: Test, read: Read<Content, Question>) {
     this.test = test
     this.read = read
   }
 
-  static create<Content>(test: number | Test<Content>, read: Content | Content[] | Read<Content>) {
+  static create<Content, Question>(test: number | Test, read: Content | Content[] | Read<Content, Question>) {
     return
   }
 }
