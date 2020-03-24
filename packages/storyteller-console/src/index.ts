641076d07ffe34e516f7e5fa40aee02b294494bd
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2020-03-24 12:26:46 +0530

    Initialized storyteller-console

---
+++ packages/storyteller-console/src/index.ts
@@ -0,0 +1,34 @@
+import {
+  WeightedSample,
+  PRNG,
+} from '@scio/storyletry-prng'
+import {
+  Context,
+  End,
+  END,
+  Question,
+  QUESTION,
+  Choice,
+  Reading,
+  Read,
+  Test,
+  Slug,
+  Storylet,
+} from '@scio/storyletry-storylet'
+import {
+  StoryletChoice,
+  StoryletterChoice,
+} from '@scio/storyletry-storyletter-choice'
+import {
+  StoryletRandom,
+  StoryletterRandom,
+} from '@scio/storyletry-storyletter-random'
+import {
+  StoryletterSequence
+} from '@scio/storyletry-storyletter-sequence'
+
+export class StorytellerConsole {
+
+}
+
+export default StorytellerConsole
