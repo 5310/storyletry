2adcfb69a62503024ff275bf67d9fca3dbeb806b
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2020-03-26 21:46:00 +0530

    Initial implementation

--- packages/storyteller-console/src/index.ts
+++ packages/storyteller-console/src/index.ts
@@ -1,36 +1,81 @@
-import produce from 'immer'
-
+import { PRNG } from '@scio/storyletry-prng'
 import {
-  WeightedSample,
-  PRNG,
-} from '@scio/storyletry-prng'
-import {
-  Context,
-  End,
   END,
-  Question,
   QUESTION,
   Choice,
-  Reading,
-  Read,
-  Test,
-  Slug,
   Storylet,
 } from '@scio/storyletry-storylet'
-import {
-  StoryletChoice,
-  StoryletterChoice,
-} from '@scio/storyletry-storyletter-choice'
-import {
-  StoryletRandom,
-  StoryletterRandom,
-} from '@scio/storyletry-storyletter-random'
-import {
-  StoryletterSequence
-} from '@scio/storyletry-storyletter-sequence'
+import * as readline from 'readline'
+
+// tslint:disable-next-line: no-console
+const write = console.log
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+const read = (prompt: string): Promise<string> =>
+  new Promise<string>(resolve => {
+    rl.question(prompt, resolve)
+  })
 
 export class StorytellerConsole {
 
+  readonly story: Storylet<string, never>
+  readonly seed: string
+
+  constructor(story: Storylet<string, never>, seed: string = '') {
+    this.story = story
+    this.seed = seed
+  }
+
+  async run() {
+    let step = 0
+    let prng = new PRNG(this.seed + step)
+    let state = {}
+    let response
+
+    do {
+
+      const reading = this.story.read({
+        prng,
+        state,
+        story: [],
+        index: [],
+        response
+      })
+
+      step++
+      state = reading.state
+      response = undefined
+
+      reading.story.forEach(section => {
+        write(section)
+        write()
+      })
+
+      if (reading.request === END) break
+      else {
+
+        prng = new PRNG(this.seed + step)
+
+        if (reading.request === QUESTION) {
+          response = read('    ?: ')
+        }
+
+        if (Array.isArray(reading.request)) {
+          reading.request.forEach((choice, i) => {
+            write(`    ${i} ${choice.slug}`)
+          })
+          const answer = await read('    ?: ')
+          write()
+          response = (reading.request as Choice<string>[])[parseInt(answer, 10)].value
+        }
+
+      }
+
+    } while (true)
+  }
 }
 
 export default StorytellerConsole
